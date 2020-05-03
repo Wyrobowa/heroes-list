eae62c90adb1f341587b61f89ab08866ccc94607
aniawyroba <aniawyroba@gmail.com>
2020-05-03 12:15:56 +0200

    added react-router-dom, added router and first route, added first container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// Containers
+import HeroesList from './containers/heroesList/HeroesList';
 
 // Styles
 import * as Styled from './appStyles';
 
 function App() {
   return (
-    <Styled.App>
-      <Styled.GlobalStyles />
-    </Styled.App>
+    <Router>
+      <Styled.App>
+        <Styled.GlobalStyles />
+        <Route exact path="/" component={HeroesList} />
+      </Styled.App>
+    </Router>
   );
 }
 
