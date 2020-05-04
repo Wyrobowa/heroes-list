f71972b973ffb1903020b4153414f8b464e6748f
aniawyroba <aniawyroba@gmail.com>
2020-05-04 11:36:09 +0200

    adjusted avatar component

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -4,18 +4,22 @@ import PropTypes from 'prop-types';
 // Styles
 import * as Styled from './avatarStyles';
 
-const Avatar = ({ src, alt, size }) => (
-  <Styled.Avatar src={src} alt={alt} size={size} />
+const Avatar = ({
+  src, alt, size, className,
+}) => (
+  <Styled.Avatar src={src} alt={alt} size={size} className={className} />
 );
 
 Avatar.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   size: PropTypes.oneOf(['small', 'medium', 'large']),
+  className: PropTypes.string,
 };
 
 Avatar.defaultProps = {
   size: 'medium',
+  className: '',
 };
 
 export default Avatar;
